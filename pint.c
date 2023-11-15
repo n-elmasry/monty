d09b2e3473e78ac0b453daaca5612161e56bdb92
Alaa Fayed <alaafayed810@gmail.com>
2023-11-15 20:07:25 +0200

    pint

--- pint.c
+++ pint.c
@@ -11,7 +11,7 @@ void pint(stack_t **stack, unsigned int line_num)
 	n_node = *stack;
 	if (n_node == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop, stack empty\n", line_num);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", n_node->n);
