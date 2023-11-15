753589b2ced89ea6f501739381ff08ca3a5eb065
Alaa Fayed <alaafayed810@gmail.com>
2023-11-15 16:20:46 +0200

    pint

--- pint.c
+++ pint.c
@@ -1,15 +1,15 @@
 #include "monty.h"
 /**
- *
- *
- *
+ * pint - prints the value at the top of the stack
+ * @stack: a pointer to node
+ * @line_num: argument
  */
 void pint(stack_t **stack, unsigned int line_num)
 {
 	stack_t *n_node;
 	n_node = *stack;
 
-	n_node = malloc(sizeof(stack_s));
+	n_node = malloc(sizeof(stack_t));
 	if (n_node == NULL)
 	{
 		print("L%u: can't pint, stack empty\n", line_number);
