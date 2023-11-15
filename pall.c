7135a40011d321be342eb2ed58528f0f25bc076c
n-elmasry <almassrynada411@gmail.com>
2023-11-15 03:55:16 -0800

    tasks

---
+++ pall.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/*pall*/
+void pall(stack_t **stack, unsigned int line_num)
+{
+    stack_t *temp = *stack;
+    UNUSED(line_num);
+    while (temp != NULL)
+    {
+        printf("%i\n", temp->n);
+        temp = temp->next;
+    }
+}
