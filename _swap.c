9b20ed9103f97f079f1bfba9c12ae77602f4e0c7
Alaa Fayed <alaafayed810@gmail.com>
2023-11-16 15:23:07 +0200

    swap

--- _swap.c
+++ _swap.c
@@ -8,8 +8,9 @@
 void swap(stack_t **stack, unsigned int line_num)
 {
 	stack_t *temp1, *temp2;
+	unsigned int temp;
 
-	if (*stack == NULL && (*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
@@ -17,13 +18,12 @@ void swap(stack_t **stack, unsigned int line_num)
 	temp1 = *stack;
 	temp2 = (*stack)->next;
 
-	temp1->prev = NULL;
-	temp2 = *stack;
-	temp1->prev = temp2;
-	temp2->next = temp1;
-	temp1->next = (*stack)->next;
+	temp = temp1->n;
+	temp1->n = temp2->n;
+	temp2->n = temp;
 	if (temp1->next != NULL)
 	{
 		temp1->next->prev = temp1;
 	}
+
 }
