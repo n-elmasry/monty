10f82550a6108717eca304162e1b2eb97d97f4e9
n-elmasry <almassrynada411@gmail.com>
2023-11-16 06:44:23 -0800

    fixing push

--- nop.c
+++ nop.c
@@ -1,6 +1,12 @@
 #include "monty.h"
+/**
+* nop - do nothing
+* @stack: a pointer to node
+* @line_num: argument
+* Return: nothing
+*/
 void nop(stack_t **stack, unsigned int line_num)
 {
-    (void)stack;
-    (void)line_num;
+(void)stack;
+(void)line_num;
 }
--- push.c
+++ push.c
@@ -1,43 +1,41 @@
 #include "monty.h"
+/**
+* push - pushes an element to the stack.
+* @stack: a pointer to node
+* @line_number: argument
+*/
 void push(stack_t **stack, unsigned int line_number)
 {
-	int value, i;
-	stack_t *new_node;
-	char *argument = strtok(NULL, " \t\n");
+int value, i;
+stack_t *new_node;
+char *argument = strtok(NULL, " \t\n");
 
-	if (argument == NULL)
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+if (argument == NULL)
+{
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
+exit(EXIT_FAILURE); }
 
+for (i = 0; argument[i] != '\0'; i++)
+{
+if (!isdigit(argument[i]) && (i == 0 && strchr("+-#", argument[i]) == NULL))
 
-	for (i = 0; argument[i] != '\0'; i++)
-	{
-		if (!isdigit(argument[i]) && (i == 0 && argument[i] != '-' && argument[i] != '+'))
-		{
-			fprintf(stderr, "L%u: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-	}
+{
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
+exit(EXIT_FAILURE); }}
 
-	value = atoi(argument);
+value = atoi(argument);
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE); }
 
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = *stack;
+new_node->n = value;
+new_node->prev = NULL;
+new_node->next = *stack;
 
-	if (*stack != NULL)
-	{
-		(*stack)->prev = new_node;
-	}
-	*stack = new_node;
-}
-
+if (*stack != NULL)
+{
+(*stack)->prev = new_node; }
+	*stack = new_node; }
