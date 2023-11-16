121ea46a14010ce6417ac9ac846bfacf58108cfc
n-elmasry <almassrynada411@gmail.com>
2023-11-15 23:59:24 -0800

    task4

--- add.c
+++ add.c
@@ -1,21 +1,28 @@
 #include "monty.h"
-
+/**
+* add - adds the top two elements of the stack.
+* @stack: a pointer to node
+* @line_num: argument
+* Return: result
+*/
 int add(stack_t **stack, unsigned int line_num)
 {
 stack_t *temp;
 int result;
 
-temp = (*stack)->next;
-if (temp == NULL)
+if (*stack == NULL || (*stack)->next == NULL)
 {
 fprintf(stderr, "L%u: can't add, stack too short\n", line_num);
 exit(EXIT_FAILURE);
 }
 
+temp = (*stack)->next;
+
 result = (*stack)->n + temp->n;
 
 (*stack)->n = result;
 (*stack)->next = temp->next;
-    free(temp);
-    return(result);
+
+free(temp);
+return (result);
 }
